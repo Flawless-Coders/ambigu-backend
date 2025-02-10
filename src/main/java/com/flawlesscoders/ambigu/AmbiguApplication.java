16b458c772fae4143f877e163c1f9f9fb24b6f2f
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2025-02-09 21:42:32 -0600

    first commit

---
+++ src/main/java/com/flawlesscoders/ambigu/AmbiguApplication.java
@@ -0,0 +1,13 @@
+package com.flawlesscoders.ambigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbiguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbiguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flawlesscoders/ambigu/AmbiguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flawlesscoders.ambigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbiguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
