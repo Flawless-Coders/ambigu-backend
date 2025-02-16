8aa84280579b3fb4c582ee58f5bd0084d59f1872
M4ltin12 <20233tn170@utez.edu.mx>
2025-02-16 12:16:25 -0600

    [ADD] CRUD ddish and category

---
+++ src/main/java/com/flawlesscoders/ambigu/menu/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.flawlesscoders.ambigu.menu;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends MongoRepository<Menu, String> {
+
+}
