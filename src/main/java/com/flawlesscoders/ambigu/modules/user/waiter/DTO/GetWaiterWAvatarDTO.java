bbb1ff4c1357f95c11d6a8ba317e77821ef8000a
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2025-02-25 07:55:26 -0600

    [ADD] GetWaiterWAvatarByEmail

---
+++ src/main/java/com/flawlesscoders/ambigu/modules/user/waiter/DTO/GetWaiterWAvatarDTO.java
@@ -0,0 +1,20 @@
+package com.flawlesscoders.ambigu.modules.user.waiter.DTO;
+
+import com.flawlesscoders.ambigu.modules.user.waiter.Shift;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetWaiterWAvatarDTO {
+    private String id;
+    private String name;
+    private String lastname_p;
+    private String lastname_m;
+    private String email;
+    private String avatarBase64;
+    private boolean isLeader;
+    private Shift shift;
+    private int AvgRating;
+}
