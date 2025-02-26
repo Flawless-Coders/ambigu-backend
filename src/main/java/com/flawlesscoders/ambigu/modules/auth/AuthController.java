0710e26759af6436088830b2b6205ee70a7ea24a
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2025-02-25 18:39:00 -0600

    [ADD] CORS permission to security

--- src/main/java/com/flawlesscoders/ambigu/modules/auth/AuthController.java
+++ src/main/java/com/flawlesscoders/ambigu/modules/auth/AuthController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
